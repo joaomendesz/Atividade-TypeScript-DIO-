9272fc792874438c1b6d95c9106c3111ee05c734
João Pedro Mendes <98474937+joaomendesz@users.noreply.github.com>
2022-06-22 15:19:54 -0300

    Em javascript

---
+++ desafio2.js
@@ -0,0 +1,20 @@
+let pessoa1 = {};
+    nome = "Juend";
+    idade = 21;
+    profissao = "Agiota"
+
+let pessoa2 = {}
+    nome = "João";
+    idade = 21;
+    profissao = "Bicheiro";  
+let pessoa3 = {
+    nome: "Cleide",
+    idade: 51,
+    profissao: "Empresária"
+};
+
+let pessoa4 = {
+    nome: "Acácio",
+    idade: 21,
+    profissao: "Analista"
+}
